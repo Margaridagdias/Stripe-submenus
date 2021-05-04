d8939acdb1df372ed82501ddd3b3179ddc0305ba
Margarida <margarida97pt@gmail.com>
2021-05-04 13:48:59 +0100

    Navbar

--- src/Hero.js
+++ src/Hero.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import phoneImg from './images/phone.svg'
-import {useGlobalContext, UseGlobalContext} from './context'
+import React from "react";
+import phoneImg from "./images/phone.svg";
+import { useGlobalContext} from "./context";
 
 const Hero = () => {
-  const data = useGlobalContext()
-  console.log(data)
-  return <h2>Hero component</h2>
-}
+  const data = useGlobalContext();
+  console.log(data);
+  return <h2>Hero component</h2>;
+};
 
-export default Hero
+export default Hero;
